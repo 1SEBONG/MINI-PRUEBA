87a9f80f3d5053c95830a451803f80cac5c4f0e5
999pryde <ma.munizaga@duocuc.cl>
2025-06-23 22:34:42 -0400

    FIN

--- parte1.py
+++ parte1.py
@@ -1,33 +1,104 @@
 import os
+
 os.system("cls")
 
+
+
 nombre = ""
+
 listaNombre = [] 
+
+
 
 while True:
 
-    while True:
-        print ("1.- Agregar alumnos")
-        print ("2.- Mostrar todos los alumnos")
-        print ("3.- Salir")
 
-        try:
-            opcion = int(input("\nIngrese su eleccion (1/2/3): "))
-            if (1 <= opcion and opcion <=3):
-                break
-            else:
-                print("Por favor seleccione las opciones mostradas (1/2/3).")
-        except ValueError:
-            print("ERROR. Ingrese un dato valido de numero entero (1/2/3).")
-    
-    if opcion == 1:
-        nombre = input("\nIngrese su nombre: ")
+
+  while True:
+
+    print ("1.- Agregar alumnos")
+
+    print ("2.- Mostrar todos los alumnos")
+
+    print ("3.- Eliminar alumno")
+
+    print ("4.- Salir")
+
+
+
+    try:
+
+      opcion = int(input("\nIngrese su eleccion (1/2/3/4): "))
+
+      if (1 <= opcion and opcion <=4):
+
+        break
+
+      else:
+
+        print("Por favor seleccione las opciones mostradas (1/2/3/4).")
+
+    except ValueError:
+
+      print("ERROR. Ingrese un dato valido de numero entero (1/2/3/4).")
+
+  
+
+  if opcion == 1:
+
+    while True:
+
+      nombre = input("\nIngrese su nombre: ")
+
+      if (nombre.isalpha()):
+
         print("Su nombre se ingreso correctamente.")
+
         listaNombre.append(nombre)
 
-
-    if opcion == 2:
-        print (listaNombre)
-
-    if opcion == 3:
         break
+
+      else:
+
+        print("\nIngrese su nombre correctamente: ") 
+
+
+
+  if opcion == 2:
+
+    print (listaNombre)
+
+
+
+  if opcion == 3:
+
+    
+
+    while True:
+
+      try:
+
+        nombreEliminar = input("\nIngrese el nombre del alumno que desea eliminar: ")
+
+        if listaNombre == nombreEliminar:
+
+          listaNombre.remove(listaNombre)
+
+          print("Alumno eliminado con exito!.")
+
+          break
+
+        else:
+
+          print("Ingrese el Nombre correctamente.")
+
+      except ValueError:
+
+          print("ERROR.Ingrese el Nombre correctamente.")    
+
+
+
+  if opcion == 4:
+
+    break
+
